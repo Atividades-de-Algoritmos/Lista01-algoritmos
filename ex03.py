74c4218854c767c8bc877cd27309410f819a9c9c
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-22 21:15:51 -0300

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,12 @@
+#
+# Crie um programa que recebe suas três notas (colégio, faculdade) e calcule a média final.
+# calcula a média
+# entrada
+nota1 = int(input("informe a nota 1: "))
+nota2 = int(input("informe a nota 2: "))
+nota3 = int(input("informe a nota 3: "))
+print(nota1 + nota2 + nota3)
+# processamento
+media = (nota1 + nota2 + nota3) / 3
+#saida
+print("a média foi ", media)
