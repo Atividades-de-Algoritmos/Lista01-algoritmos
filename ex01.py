ea95e8391a521c0d1ba4c979085856216b6453b4
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-22 21:15:39 -0300

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,11 @@
+#
+# Faça um algoritmo de conversão de uma temperatura em graus celsius
+# para uma temperatura em Farenheit.   Formula: 9/5(celsius) = farenhit – 32
+
+# entrada de dados
+celsius = float(input("informe a temperatura em celsius: "))
+# processamento
+farenhit = 9/5 * celsius + 32
+# saída de dados
+print(f"A temperatura em farenheit é: {farenhit}")
+print('fim do programa')
