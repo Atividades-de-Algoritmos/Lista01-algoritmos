7f4ce766077eaca7c569bc35ae3959bbb4db66df
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 00:10:30 -0300

    IMC 📋

--- ex04.py
+++ ex04.py
@@ -4,13 +4,13 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 27/06/2022
 #
 # 4. Crie um programa que receba a altura e o peso do usuário
 # e diga seu IMC (Índice de Massa Corporal), dado pela       
 # fórmula: IMC = peso / (altura ** 2).                       
 
-# entrada de dados
+# Entrada de dados
 
 peso = int(input("Digite o peso: ")) # Recebendo o valor inteiro do peso
 altura = float(input("Digite a altura: ")) # Recebendo o valor flutuante da altura
