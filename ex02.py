18b7e14dcc06f0efb9910dbe97e75045b34321c2
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 00:08:14 -0300

    Aprendendo variáveis ❌

--- ex02.py
+++ ex02.py
@@ -11,20 +11,20 @@
 # que A fique com o valor de B, B com o valor de C e C com o 
 # valor de A. Mostre os valores finais.                      
 
-# entrada de dados
+# Entrada de dados
 
 A = input("informe um valor para o A: ") # Usamos input() para saber o valor de A
 B = input("informe um valor para o B: ") # Usamos input() para saber o valor de B
 C = input("informe um valor para o C: ") # Usamos input() para saber o valor de C
 
-# processamento de dados
+# Processamento de dados
 
 X = A # X vai recebe o valor da variável A
 A = B # A vai recebe o valor da variável B
 B = C # B vai recebe o valor da variável C
 C = X # C vai recebe o valor da variável X
 
-# saída de dados
+# Saída de dados
 
 print("o valor de A =", A) # Imprimindo no terminal o valor de A
 print("o valor de B =", B) # Imprimindo no terminal o valor de B
