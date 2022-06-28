91b60c49dc08c66459621e3267982591b47c517d
Carlos Eduardo ✔ <97265556+SmokeDevL@users.noreply.github.com>
2022-06-28 00:05:10 -0300

    Conversor de temperatura 🌡

--- ex01.py
+++ ex01.py
@@ -4,20 +4,21 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 27/06/2022
 #
 # 1. Faça um algoritmo de conversão de uma temperatura em    
-# graus Celsius para uma temperatura em Fahrenheit.          
+# graus Celsius para uma temperatura em Fahrenheit com a     
+# a fórmula de conversão f = 9/5 * c + 32                          
 
-# entrada de dados
+# Entrada de dados
 
-celsius = float(input("informe a temperatura em celsius: ")) # lemos a temperatura em Celsius informada pelo usuário
+celsius = float(input("informe a temperatura em celsius: ")) # Lemos a temperatura em Celsius informada pelo usuário
 
-# processamento
+# Processamento de dados
 
-fahrenheit = 9/5 * celsius + 32 # calculamos a temperatura em fahrenheit usando a fórmula da conversão de temperatura em Celsius para fahrenheit.
+fahrenheit = 9/5 * celsius + 32 # Calculamos a temperatura em fahrenheit usando a fórmula da conversão de temperatura em Celsius para fahrenheit.
 
 # saída de dados
 
-print(f"A temperatura em fahrenheit é: {fahrenheit}") # imprimimos a temperatura em fahrenheit
+print(f"A temperatura em fahrenheit é: {fahrenheit}") # Imprimimos a temperatura em fahrenheit
 print('fim do programa') # Informamos ao usuário que o programa terminou
